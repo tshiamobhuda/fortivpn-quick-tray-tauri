d2b72ef36761fb13033c630c9ba1f0603757986c
Tshiamo Bhuda <tshiamobhuda@gmail.com>
2022-11-06 01:56:00 +0200

    Adds ESLint

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  clearScreen: false,
-  server: {
-    strictPort: true,
-  },
-  envPrefix: ['VITE_', 'TAURI'],
-  build: {
-    target: ['es2021'],
-    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
-    sourcemap: !!process.env.TAURI_DEBUG,
-  },
-  plugins: [vue()]
-})
+    clearScreen: false,
+    server: {
+        strictPort: true,
+    },
+    envPrefix: [ 'VITE_', 'TAURI' ],
+    build: {
+        target: [ 'es2021' ],
+        minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+        sourcemap: !!process.env.TAURI_DEBUG,
+    },
+    plugins: [ vue(), eslintPlugin() ],
+});
