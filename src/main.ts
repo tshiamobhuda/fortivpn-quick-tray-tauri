771adfd6083b3e3320e706970d50a37271f515a2
Tshiamo Bhuda <tshiamobhuda@gmail.com>
2022-11-06 02:48:47 +0200

    Adds Quasar

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,26 @@
 import { createApp } from 'vue';
+import { Quasar } from 'quasar';
+import quasarIconSet from 'quasar/icon-set/svg-material-icons';
+
+// Import icon libraries
+import '@quasar/extras/roboto-font/roboto-font.css';
+import '@quasar/extras/material-icons/material-icons.css';
+
+// Import Quasar css
+import 'quasar/src/css/index.sass';
+
 import './style.css';
+
+// Assumes your root component is App.vue
+// and placed in same folder as main.js
 import App from './App.vue';
 
-createApp(App).mount('#app');
+const myApp = createApp(App);
+
+myApp.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+    iconSet: quasarIconSet,
+});
+
+// Assumes you have a <div id="app"></div> in your index.html
+myApp.mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import eslintPlugin from 'vite-plugin-eslint';
+import { quasar,transformAssetUrls } from '@quasar/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,11 +9,19 @@ export default defineConfig({
     server: {
         strictPort: true,
     },
-    envPrefix: [ 'VITE_', 'TAURI' ],
+    envPrefix: [ 'VITE_','TAURI' ],
     build: {
         target: [ 'es2021' ],
         minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
         sourcemap: !!process.env.TAURI_DEBUG,
     },
-    plugins: [ vue(), eslintPlugin() ],
+    plugins: [
+        vue({
+            template: { transformAssetUrls },
+        }),
+        eslintPlugin(),
+        quasar({
+            sassVariables: 'src/assets/quasar-variables.sass',
+        }),
+    ],
 });
